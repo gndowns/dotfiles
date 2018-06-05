3fcae79eb3c124ebb5695bc00ddaee72db8ba5f0
Gabriel Downs <gndowns@gmail.com>
2018-06-05 14:05:38 -0400

    python tabs fixed

---
+++ python.vim
@@ -0,0 +1,4 @@
+" python tabs => 2 spaces
+setlocal expandtab
+setlocal shiftwidth=2
+setlocal tabstop=2
--- symlink.sh
+++ symlink.sh
@@ -3,15 +3,21 @@
 # Script to symlink all config files in /dotfiles
 # NOTE: will overwrite current local files
 
-dotfiles=('.vimrc' '.screenrc' '.zshrc')
+dotfiles=('.vimrc' '.screenrc' '.zshrc', 'python.vim')
+# locations for links
+targets=("$HOME/.vimrc" "$HOME/.screenrc" "$HOME/.zshrc"
+  "$HOME/.vim/after/ftplugin/python.vim")
 
-for f in "${dotfiles[@]}"; do
+# for f in "${dotfiles[@]}"; do
+for ((i=0; i<${#dotfiles[@]}; i++)); do
+  f="${dotfiles[i]}"
+  t="${targets[i]}"
   # ask permission to overwrite existing file
-  read -p "Are you sure you want to overwrite $HOME/$f ? [y/n] " yn
+  read -p "Are you sure you want to overwrite $t ? [y/n] " yn
   if [[ "$yn" == 'y' ]]; then
     # remove old file (to avoid 'file exists' error)
-    rm $HOME/$f
+    rm -f $t
     # symbolic link target link_name
-    ln -s $HOME/dotfiles/$f $HOME/$f
+    ln -s $HOME/dotfiles/$f $t
   fi
 done
