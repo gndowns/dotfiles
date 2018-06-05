a8f8cec6ce22992e4c2c0a8220ef02798a3c588f
Gabriel Downs <gndowns@gmail.com>
2018-06-05 14:27:59 -0400

    fix symlinking

--- symlink.sh
+++ symlink.sh
@@ -3,21 +3,20 @@
 # Script to symlink all config files in /dotfiles
 # NOTE: will overwrite current local files
 
-dotfiles=('.vimrc' '.screenrc' '.zshrc', 'python.vim')
+targets=('.vimrc' '.screenrc' '.zshrc' 'python.vim')
 # locations for links
-targets=("$HOME/.vimrc" "$HOME/.screenrc" "$HOME/.zshrc"
+links=("$HOME/.vimrc" "$HOME/.screenrc" "$HOME/.zshrc"
   "$HOME/.vim/after/ftplugin/python.vim")
 
-# for f in "${dotfiles[@]}"; do
-for ((i=0; i<${#dotfiles[@]}; i++)); do
-  f="${dotfiles[i]}"
+for ((i=0; i<${#targets[@]}; i++)); do
   t="${targets[i]}"
+  l="${links[i]}"
   # ask permission to overwrite existing file
-  read -p "Are you sure you want to overwrite $t ? [y/n] " yn
+  read -p "Are you sure you want to overwrite $l ? [y/n] " yn
   if [[ "$yn" == 'y' ]]; then
     # remove old file (to avoid 'file exists' error)
-    rm -f $t
+    rm -f $l
     # symbolic link target link_name
-    ln -s $HOME/dotfiles/$f $t
+    ln -s $HOME/dotfiles/$t $l
   fi
 done
